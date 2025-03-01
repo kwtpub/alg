0e6161357ba21b4a6700e4582e9e54bfc9f6be25
kwtpub <10.0.kwt@mail.ru>
2025-03-01 15:28:51 +0400

    ss

---
+++ FunctionComposition.js
@@ -0,0 +1,10 @@
+
+functions = [x => x + 1, x => x * x, x => 2 * x], x=4
+
+
+var compose = function(functions) {
+    
+    return function(x) {
+        
+    }
+};
--- arrayReduce.js
+++ arrayReduce.js
@@ -1,10 +1,10 @@
-nums = [1,2,3,4,5]
+nums = [1,2,3,4]
 fn = function sum(accum, curr) { return accum + curr; }
 init = 0
 var reduce = function(nums, fn, init) {
-    let result = []
-    for(let i = 0; i < nums.langth; i++) { 
-        result += fn(init,nums[i])
+    let result = init
+    for(let i = 0; i < nums.length; i++) { 
+        result = fn(result, nums[i])
     }
     return result
 };
