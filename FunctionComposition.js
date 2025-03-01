ee53904fcc1a8f97a510327b4c671dce3bf82c11
kwtpub <10.0.kwt@mail.ru>
2025-03-01 15:43:26 +0400

    initial

--- FunctionComposition.js
+++ FunctionComposition.js
@@ -1,10 +1,17 @@
 
-functions = [x => x + 1, x => x * x, x => 2 * x], x=4
+functions = [x => x + 1, x => x * x, x => 2 * x]
 
 
 var compose = function(functions) {
-    
+
     return function(x) {
-        
+        let val = x;
+        for(let i = functions.length - 1; i > -1 ; i-- ) {
+            val = functions[i](val)
+        }
+        return val
     }
 };
+fn = compose(functions)
+
+console.log(fn(4))
